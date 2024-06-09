59db03cc294d0e4b243fd27b9a036b5b6da208b2
Joumana <joumanasalahedin@gmail.com>
2024-06-10 00:50:12 +0200

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Type-annotated function add"""
+
+def add(a: float, b: float) -> float:
+    """arguments: float a and float b
+    Returns sum as a float"""
+    return a + b
