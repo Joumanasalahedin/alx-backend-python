5b545ad9cb237f206684b2baf37ada60cee380f1
Joumana <joumanasalahedin@gmail.com>
2024-06-10 01:38:16 +0200

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Type-annotated function make_multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier"""
+    def multi(num: float) -> float:
+        """multiplies a float by multiplier"""
+        return multiplier * num
+    
+    return multi
