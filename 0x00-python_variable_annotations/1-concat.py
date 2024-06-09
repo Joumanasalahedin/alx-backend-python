3a0dea5463c707d26f9cfdc055b3cff43f636952
Joumana <joumanasalahedin@gmail.com>
2024-06-10 01:03:55 +0200

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,9 +5,3 @@
 def concat(str1: str, str2: str) -> str:
     """Returns a concatenated string"""
     return f"{str1}{str2}"
-
-str1 = "egg"
-str2 = "shell"
-
-print(concat(str1, str2) == "{}{}".format(str1, str2))
-print(concat.__annotations__)
