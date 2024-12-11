31e920d4b0040ba311c6640b6f8bd937337d2055
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-12-11 10:43:54 +0100

    task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Unittests for Client"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test suite for the GithubOrgClient class."""
+
+    @parameterized.expand([
+        ("google", {"login": "google"}),
+        ("abc", {"login": "abc"}),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, expected_response, mock_get_json):
+        """
+        Test that GithubOrgClient.org returns the correct value.
+        Ensure get_json is called once with the expected argument.
+        """
+        mock_get_json.return_value = expected_response
+
+        client = GithubOrgClient(org_name)
+
+        self.assertEqual(client.org, expected_response)
+
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+
+
+if __name__ == "__main__":
+    unittest.main()
