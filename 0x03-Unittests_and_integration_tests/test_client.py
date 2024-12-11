8883e72cd436953c4d9222bbee5b192feac4923c
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-12-11 10:45:49 +0100

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unittests for Client"""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -16,18 +16,24 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch("client.get_json")
     def test_org(self, org_name, expected_response, mock_get_json):
-        """
-        Test that GithubOrgClient.org returns the correct value.
-        Ensure get_json is called once with the expected argument.
-        """
+        """Test that GithubOrgClient.org returns the correct value."""
         mock_get_json.return_value = expected_response
-
         client = GithubOrgClient(org_name)
-
         self.assertEqual(client.org, expected_response)
-
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
+
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test that GithubOrgClient._public_repos_url returns the correct value."""
+        mock_org.return_value = {
+            "repos_url": "https://api.github.com/orgs/google/repos"}
+
+        client = GithubOrgClient("google")
+        result = client._public_repos_url
+
+        self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+        mock_org.assert_called_once()
 
 
 if __name__ == "__main__":
